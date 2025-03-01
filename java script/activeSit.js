031553d3c12fbfaaaee42bce65253a15df10cf12
Md Asfier Rahman Marjan <mdmarjan2233@gmail.com>
2025-03-01 19:33:20 +0600

    Added Java script & other some pblm

---
+++ java script/activeSit.js
@@ -0,0 +1,146 @@
+const AllOutput = document.getElementById('All-history');
+document.getElementById('clear-history').addEventListener('click', 
+    function(event){
+        event.preventDefault();
+        AllOutput.innerText = '';
+    }
+)
+// 1st active history js
+const outputHeading = document.getElementById('active-text');
+document.getElementById('card-btn').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        this.setAttribute("disabled", true);
+        this.classList.add("opacity-20",);
+        alert("Board updated Successfully");
+        const newTime = new Date().toLocaleTimeString();
+        const cardHeading = document.getElementById('1stCardHeading').innerText;
+        outputHeading.innerText = `You have Complete The Task ${cardHeading} at ${newTime}`;
+        // task count section
+        const taskCount = document.getElementById('task-count').innerText;
+        const convertTaskCount = parseFloat(taskCount);
+        sum = convertTaskCount - 1 ;
+        document.getElementById('task-count').innerText = sum;
+        // navCount section
+        const navCount = document.getElementById('navCount').innerText;
+        const convertnavCount = parseFloat(navCount);
+        navSum = convertnavCount + 1 ;
+        document.getElementById('navCount').innerText = navSum;
+    }
+)
+// 2nd active history js
+const outputHeading2 = document.getElementById('active-text2');
+document.getElementById('card-btn2').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        this.setAttribute("disabled", true);
+        this.classList.add("opacity-20",);
+        alert("Board updated Successfully");
+        const newTime = new Date().toLocaleTimeString();
+        const cardHeading = document.getElementById('2ndCardHeading').innerText;
+        outputHeading2.innerText = `You have Complete The Task ${cardHeading} at ${newTime}`;
+        // task count section
+        const taskCount = document.getElementById('task-count').innerText;
+        const convertTaskCount = parseFloat(taskCount);
+        sum = convertTaskCount - 1 ;
+        document.getElementById('task-count').innerText = sum;
+        // navCount section
+        const navCount = document.getElementById('navCount').innerText;
+        const convertnavCount = parseFloat(navCount);
+        navSum = convertnavCount + 1 ;
+        document.getElementById('navCount').innerText = navSum;
+    }
+)
+// 3rd active history js
+const outputHeading3 = document.getElementById('active-text3');
+document.getElementById('card-btn3').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        this.setAttribute("disabled", true);
+        this.classList.add("opacity-20",);
+        alert("Board updated Successfully");
+        const newTime = new Date().toLocaleTimeString();
+        const cardHeading = document.getElementById('3ndCardHeading').innerText;
+        outputHeading3.innerText = `You have Complete The Task ${cardHeading} at ${newTime}`;
+        // task count section
+        const taskCount = document.getElementById('task-count').innerText;
+        const convertTaskCount = parseFloat(taskCount);
+        sum = convertTaskCount - 1 ;
+        document.getElementById('task-count').innerText = sum;
+        // navCount section
+        const navCount = document.getElementById('navCount').innerText;
+        const convertnavCount = parseFloat(navCount);
+        navSum = convertnavCount + 1 ;
+        document.getElementById('navCount').innerText = navSum;
+    }
+)
+// 4th active history js
+const outputHeading4 = document.getElementById('active-text4');
+document.getElementById('card-btn4').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        this.setAttribute("disabled", true);
+        this.classList.add("opacity-20",);
+        alert("Board updated Successfully");
+        const newTime = new Date().toLocaleTimeString();
+        const cardHeading = document.getElementById('4ndCardHeading').innerText;
+        outputHeading4.innerText = `You have Complete The Task ${cardHeading} at ${newTime}`;
+        // task count section
+        const taskCount = document.getElementById('task-count').innerText;
+        const convertTaskCount = parseFloat(taskCount);
+        sum = convertTaskCount - 1 ;
+        document.getElementById('task-count').innerText = sum;
+        // navCount section
+        const navCount = document.getElementById('navCount').innerText;
+        const convertnavCount = parseFloat(navCount);
+        navSum = convertnavCount + 1 ;
+        document.getElementById('navCount').innerText = navSum;
+    }
+)
+// 5th active history js
+const outputHeading5 = document.getElementById('active-text5');
+document.getElementById('card-btn5').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        this.setAttribute("disabled", true);
+        this.classList.add("opacity-20",);
+        alert("Board updated Successfully");
+        const newTime = new Date().toLocaleTimeString();
+        const cardHeading = document.getElementById('5ndCardHeading').innerText;
+        outputHeading5.innerText = `You have Complete The Task  ${cardHeading} at ${newTime}`;
+        // task count section
+        const taskCount = document.getElementById('task-count').innerText;
+        const convertTaskCount = parseFloat(taskCount);
+        sum = convertTaskCount - 1 ;
+        document.getElementById('task-count').innerText = sum;
+        // navCount section
+        const navCount = document.getElementById('navCount').innerText;
+        const convertnavCount = parseFloat(navCount);
+        navSum = convertnavCount + 1 ;
+        document.getElementById('navCount').innerText = navSum;
+    }
+)
+// 6th active history js
+const outputHeading6 = document.getElementById('active-text6');
+document.getElementById('card-btn6').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        this.setAttribute("disabled", true);
+        this.classList.add("opacity-20",);
+        alert("Board updated Successfully");
+        alert("congrates!!! You have completed All the current task");
+        const newTime = new Date().toLocaleTimeString();
+        const cardHeading = document.getElementById('6ndCardHeading').innerText;
+        outputHeading6.innerText = `You have Complete The Task  ${cardHeading} at ${newTime}`;
+        // task count section
+        const taskCount = document.getElementById('task-count').innerText;
+        const convertTaskCount = parseFloat(taskCount);
+        sum = convertTaskCount - 1 ;
+        document.getElementById('task-count').innerText = sum;
+        // navCount section
+        const navCount = document.getElementById('navCount').innerText;
+        const convertnavCount = parseFloat(navCount);
+        navSum = convertnavCount + 1 ;
+        document.getElementById('navCount').innerText = navSum;
+    }
+)
