098ec6c1ac700caaf995b421a3f5314244f67c59
Md Asfier Rahman Marjan <mdmarjan2233@gmail.com>
2025-03-02 16:27:25 +0600

    Added final new html file & some others pblm

--- java script/back.js
+++ java script/back.js
@@ -1,7 +1,7 @@
 document.getElementById('backBtn').addEventListener('click',
     function(event){
         event.preventDefault();
-         window.location.href="../index.html"
+         window.location.href="../JavaScript-Assignment-5/index.html"
     }
 )
 
