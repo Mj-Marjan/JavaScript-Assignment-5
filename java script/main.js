f7ac151f13673be25376a7b1fb6b04edac2b9f7e
Md Asfier Rahman Marjan <mdmarjan2233@gmail.com>
2025-03-02 16:23:34 +0600

    Added new html file & some others pblm sol 2v

--- java script/main.js
+++ java script/main.js
@@ -1,7 +1,7 @@
 document.getElementById('calender-btn').addEventListener('click',
     function(event){
         event.preventDefault();
-         window.location.href="../main.html"
+         window.location.href="../JavaScript-Assignment-5/main.html"
     }
 )
 
